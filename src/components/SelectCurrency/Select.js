5bd633f3c139e51a22699df31339ebcf25c7dcc3
huzeyfereis <huzeyfereis@hotmail.com>
2020-08-29 01:32:27 +0100

    Create currency selection components

---
+++ src/components/SelectCurrency/Select.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export default styled.select`
+  line-height: 1em;
+  background-color: transparent;
+  border-style: none;
+  color: white;
+  font-size: 16px;
+  border-bottom: 1px solid #dfdfdf;
+  color: #6a6c84;
+`
---
+++ src/components/SelectCurrency/SelectCurrency.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+export default styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  width: 100%;
+  margin: 0 auto;
+  padding: 0;
+  height: 60px;
+  margin-right: 8px;
+
+  option {
+    background: white;
+    font-weight: small;
+    display: flex;
+    white-space: pre;
+    min-height: 20px;
+    padding: 0px 2px 1px;
+    color: #6a6c84
+  }
+`
