be46dd09b6c37fd42511201e71ddb87082ed301f
huzeyfereis <huzeyfereis@hotmail.com>
2020-08-29 01:31:36 +0100

    Create logo component

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export default styled.div`
+  width: 100%;
+  max-width: 200px;
+  padding: 16px;
+  font-size: x-large;
+  color: #6a6c84;
+  font-family: 'montserrat';
+`
