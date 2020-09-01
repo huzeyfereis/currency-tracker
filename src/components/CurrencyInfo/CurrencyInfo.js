b450600ee4d0f4cb6c1f08798405300959a462ec
huzeyfereis <huzeyfereis@hotmail.com>
2020-09-01 08:19:54 +0100

    Create currency info components

---
+++ src/components/CurrencyInfo/CurrencyInfo.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export default styled.div`
+  width: 100%;
+  max-width: 400px;
+  padding: 16px;
+  font-size: large;
+  color: #6a6c84;
+  font-family: 'montserrat';
+  margin-left: 15px;
+`
