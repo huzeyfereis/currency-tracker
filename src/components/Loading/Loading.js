f0beddd612d52824fd53738373adcc7c9b4e33d3
huzeyfereis <huzeyfereis@hotmail.com>
2020-09-01 08:22:19 +0100

    Create loading component

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+import { HashLoader } from 'react-spinners'
+
+const LoadingDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 120px;
+`
+const Loading = () => (
+  <LoadingDiv>
+    <HashLoader size={80} color={'#5993f1'} loading />
+  </LoadingDiv>
+)
+
+export default Loading
