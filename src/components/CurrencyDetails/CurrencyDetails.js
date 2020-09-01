ee35da723d098b4b519ca90ec0b26ebbd6642075
huzeyfereis <huzeyfereis@hotmail.com>
2020-09-01 08:19:31 +0100

    Create currency details components

---
+++ src/components/CurrencyDetails/CurrencyDetails.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export default styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  width: 100%;
+  margin: 0 auto;
+  padding: 0;
+  background-color: #1a2f4e;
+  height: 500px;
+`
---
+++ src/components/CurrencyDetails/components/Details/Details.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export default styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  margin: 0 auto;
+  padding: 0;
+  height: auto;
+  margin-right: 8px;
+  font-family: 'montserrat';
+`
